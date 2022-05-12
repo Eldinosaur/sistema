e86c9e6f547ba7a38c84f28782ee28452a6bed79
Eldinosaur <eldinosaur7u7@outlook.com>
2022-05-12 18:34:46 -0500

    container

--- projects/app-qr/src/app/shared/shared.module.ts
+++ projects/app-qr/src/app/shared/shared.module.ts
@@ -5,22 +5,28 @@ import { ContainerComponent } from './components/container/container.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
+import {MatCardModule} from '@angular/material/card';
+import {MatTableModule} from '@angular/material/table';
+import { TableComponent } from './components/table/table.component';
 
 @NgModule({
   declarations: [
     TitleComponent,
-    ContainerComponent
+    ContainerComponent,
+    TableComponent
   ],
   imports: [
     CommonModule,
     MatToolbarModule,
     MatIconModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatCardModule,
+    MatTableModule
   ],
   exports: [
     TitleComponent,
-    ContainerComponent
+    ContainerComponent,
+    TableComponent
   ]
 })
 export class SharedModule { }
