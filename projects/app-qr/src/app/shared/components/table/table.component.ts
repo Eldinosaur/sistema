9ca84a6dca929a95db21d9d88333e79d4302e77b
Eldinosaur <eldinosaur7u7@outlook.com>
2022-05-12 18:50:53 -0500

    componente table de agencias

--- projects/app-qr/src/app/shared/components/table/table.component.ts
+++ projects/app-qr/src/app/shared/components/table/table.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+interface IData{
+  id: number,
+  agency: string
+}
+
 @Component({
   selector: 'qr-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit {
+  data:IData[]=[
+    {id:1,agency:'Ambato'},
+    {id:2,agency:'Quito'},
+    {id:3,agency:'Guayaquil'},
+    {id:4,agency:'Cuenca'},
+    {id:5,agency:'Riobamba'}
+  ]
+  listFields:string[]=['id','agency']
 
   constructor() { }
 
